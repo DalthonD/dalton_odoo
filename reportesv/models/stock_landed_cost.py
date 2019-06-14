3c50e298a830bcee1ec7c272751ef430a3183765
DalthonD <49283389+DalthonD@users.noreply.github.com>
2019-06-14 10:26:49 -0600

    Wizard para reporte de retaceo

---
+++ reportesv/models/stock_landed_cost.py
@@ -0,0 +1,16 @@
+import logging
+from odoo import fields, models, api, SUPERUSER_ID, _
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo import tools
+import pytz
+from pytz import timezone
+from datetime import datetime, date, timedelta
+from odoo.exceptions import UserError, ValidationError
+from odoo import exceptions
+_logger = logging.getLogger(__name__)
+
+class stock_landed_cost(models.Model):
+    _name = "stock_landed_cost"
+    _inherit = "stock_landed_cost"
+
+    
