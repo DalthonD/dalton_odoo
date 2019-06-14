c5fbd029e669ab0fb462d3cab2d8e41d8a901ef1
DalthonD <49283389+DalthonD@users.noreply.github.com>
2019-06-14 11:15:08 -0600

    Many2many field en wizard

--- reportesv/wizard/wizard_landed_cost.py
+++ reportesv/wizard/wizard_landed_cost.py
@@ -9,7 +9,7 @@ class wizard_sv_stock_landed_cost_report(models.TransientModel):
     _name = 'wizard.sv.stock.landed.cost.report'
 
     company_id=fields.Many2one('res.company', string="Company", help='Company',default=lambda self: self.env.user.company_id.id)
-    stock_landed_cost_ids=fields.Many2one('stock.landed.cost', string="Costos de envío", help='Costos incurridos en la adquisición de mercancía')
+    stock_landed_cost_ids=fields.Many2many('stock.landed.cost', 'stock_landed_cost_list', 'wizard_id','cost_id', string="Costos de envío", help='Costos incurridos en la adquisición de mercancía')
 
     @api.multi
     def print_landed_cost_report(self):
