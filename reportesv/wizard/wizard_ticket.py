77b899bf63d77befad5cee2620e73dd3b3c12d9f
DalthonD <49283389+DalthonD@users.noreply.github.com>
2019-07-01 11:25:59 -0600

    Update

--- reportesv/wizard/wizard_ticket.py
+++ reportesv/wizard/wizard_ticket.py
@@ -11,7 +11,7 @@ class wizard_sv_ticket_report(models.TransientModel):
     company_id=fields.Many2one('res.company', string="Company", help='Company',default=lambda self: self.env.user.company_id.id)
     date_month = fields.Selection([('1','Enero'),('2','Febrero'),('3','Marzo'),('4','Abril'),('5','Mayo'),('6','Junio'),('7','Julio'),('8','Agosto'),('9','Septiembre'),('10','Octubre'),('11','Noviembre'),('12','Diciembre')],string='Mes de facturación', default='5',required=True)
     date_year = fields.Integer("Año de facturación", default=2019, requiered=True)
-    stock_location_id=fields.Many2one('stock.location', string="Sucursal", help="Sucursal de la que se desea el Libro de IVA",default=lambda self: self.env.user.sucursal_id.id)
+    #stock_location_id=fields.Many2one('stock.location', string="Sucursal", help="Sucursal de la que se desea el Libro de IVA",default=lambda self: self.env.user.sucursal_id.id)
 
     @api.multi
     def print_ticket_sales_report(self):
